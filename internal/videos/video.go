33792a00c8fee934e6f1358884f0dfd39e0d7b25
Noel Yahan <noelyahan@gmail.com>
2019-04-28 21:08:57 +0530

    refactor code, reduce the frame load code

--- internal/videos/video.go
+++ internal/videos/video.go
@@ -8,11 +8,11 @@ import (
 	"github.com/noelyahan/mergitrans/internal/io"
 )
 
-
-func getFrames(name string, from, to int) []image.Image {
+// GetFrames is used to load video frames
+func GetFrames(name string, from, to int) []image.Image {
 	frames := make([]image.Image, 0)
 	_, filename, _, _ := runtime.Caller(1)
-	path := filepath.Join(filepath.Dir(filename), name)
+	path := filepath.Join(filepath.Dir(filename), "internal", "videos", name)
 	for i := from; i <= to; i++ {
 		p := fmt.Sprintf("%s/frame%d.jpg", path, int(i))
 		img, _ := io.NewFileImporter(p).Import()
@@ -20,7 +20,3 @@ func getFrames(name string, from, to int) []image.Image {
 	}
 	return frames
 }
-
-var PoppyField = getFrames("1", 1, 150)
-var Bird = getFrames("2", 1, 150)
-var Clouds = getFrames("3", 1, 150)
--- trans.go
+++ trans.go
@@ -9,16 +9,19 @@ import (
 // bonus video frames
 var Videos = struct {
 	PoppyField []image.Image
-	Bird []image.Image
-	Clouds []image.Image
-}{videos.PoppyField, videos.Bird, videos.Clouds}
+	Bird       []image.Image
+	Clouds     []image.Image
+}{
+	videos.GetFrames("1", 1, 150),
+	videos.GetFrames("2", 1, 150),
+	videos.GetFrames("3", 1, 150)}
 
 // Support transitions
-var SlideBar = internal.GetSlideBar()
-var Ink1 = internal.GetInk1()
-var Ink2 = internal.GetInk2()
-var Ink3 = internal.GetInk3()
-var ScaleUpFastRect = internal.GetScaleUpFastRect()
-var ScaleDownFastRect = internal.GetScaleDownFastRect()
-var ScaleUpFastCircle = internal.GetScaleUpFastCircle()
-var ScaleDownFastCircle = internal.GetScaleDownFastCircle()
+var SlideBar = internal.GetFrames("slide_bar", 1, 36)
+var Ink1 = internal.GetFrames("transition_ink", 1, 60)
+var Ink2 = internal.GetFrames("transition_ink", 61, 98)
+var Ink3 = internal.GetFrames("transition_ink", 99, 150)
+var ScaleUpFastRect = internal.GetFrames("scale_rect", 39, 69)
+var ScaleDownFastRect = internal.GetFrames("scale_rect", 1, 39)
+var ScaleUpFastCircle = internal.GetFrames("scale_circle", 39, 69)
+var ScaleDownFastCircle = internal.GetFrames("scale_circle", 1, 39)
